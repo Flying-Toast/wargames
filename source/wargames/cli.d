dff050d97d2743694cd7e043adf2587ce2ac69de
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-11-17 17:56:07 -0500

    list games and help games

--- source/wargames/cli.d
+++ source/wargames/cli.d
@@ -29,7 +29,63 @@ class Cli {
 
 		clearScreen();
 		write("LOGON: ");
-		if (readln().replace("\n", "").toLower != "joshua") {
+
+		string line = readln().replace("\n", "").toLower;
+		while (line == "joshua" || line == "help logon" || line == "help games" || line == "list games") {
+			if (line == "joshua") {
+				writeln();
+				break;
+			}
+
+			if (line == "help games") {
+				writeln();
+				typeText("'GAMES' REFERES TO MODELS, SIMULATIONS, AND GAMES", "helpgames", 45);
+				typeText("WHICH HAVE TACTICAL AND STRATEGIC APPLICATIONS.", "", 45);
+			}
+
+			if (line == "help logon") {
+				writeln();
+				typeText("HELP NOT AVAILABLE", "nohelp", 45);
+			}
+
+			if (line == "list games") {
+				writeln();
+				typeText("FALKENS MAZE", "falkensmaze", 45);
+				Thread.sleep(dur!"msecs"(1500));
+				typeText("BLACK JACK", "blackjack", 45);
+				Thread.sleep(dur!"msecs"(600));
+				typeText("GIN RUMMY", "rummy", 45);
+				Thread.sleep(dur!"msecs"(600));
+				typeText("HEARTS", "hearts", 45);
+				Thread.sleep(dur!"msecs"(400));
+				typeText("BRIDGE", "bridge", 45);
+				Thread.sleep(dur!"msecs"(300));
+				typeText("CHECKERS", "checkers", 45);
+				Thread.sleep(dur!"msecs"(500));
+				typeText("CHESS", "chessgame", 45);
+				Thread.sleep(dur!"msecs"(300));
+				typeText("POKER", "poker", 45);
+				Thread.sleep(dur!"msecs"(200));
+				typeText("FIGHTER COMBAT", "fightercombat", 45);
+				Thread.sleep(dur!"msecs"(100));
+				typeText("GUERRILLA ENGAGEMENT", "engagement", 45);
+				Thread.sleep(dur!"msecs"(250));
+				typeText("DESERT WARFARE", "desert", 45);
+				Thread.sleep(dur!"msecs"(1100));
+				typeText("AIR-TO-GROUND ACTIONS", "actions", 20);
+				Thread.sleep(dur!"msecs"(800));
+				typeText("THEATERWIDE TACTICAL WARFARE", "tactical", 45);
+				Thread.sleep(dur!"msecs"(1000));
+				typeText("THEATERWIDE BIOTOXIC AND CHEMICAL WARFARE", "biotoxic", 45);
+				Thread.sleep(dur!"msecs"(1300));
+				typeText("GLOBAL THERMONUCLEAR WAR", "global", 45);
+			}
+
+			write("LOGON: ");
+			line = readln().replace("\n", "").toLower;
+		}
+		if (line != "joshua") {
+			writeln();
 			typeText("IDENTIFICATION NOT RECOGNIZED BY SYSTEM", "terminated", 35);
 			typeText("--CONNECTION TERMINATED--", "", 25);
 			return;
